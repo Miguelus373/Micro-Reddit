27604d7b023ec83abd3d3432c2d56b3e6a8ccec1
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-10-06 11:50:39 -0400

    Corrected database references

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_10_05_222752) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,4 +28,5 @@ ActiveRecord::Schema.define(version: 2020_10_05_222752) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
